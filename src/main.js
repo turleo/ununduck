5a19838150d938f03b838eb09bb80dc8992d1fd8
Valya Tureyskaya <me@turleo.me>
2025-11-18 01:04:21 +1000

    ⚡ redirects now served from service worker

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
+async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
 function noSearchDefaultPageRender() {
+  registerServiceWorker();
   const copyButton = document.querySelector(".copy-button");
   const copyIcon = copyButton.querySelector("img");
   const urlInput = document.querySelector(".url-input");
@@ -13,74 +26,5 @@ function noSearchDefaultPageRender() {
   });
 }
 
-async function fetchBangs() {
-  const response = await fetch("assets/bangs.json");
-  return await response.json();
-}
-
-
-async function getBangredirectUrl() {
-  const url = new URL(window.location.href);
-  const query = (url.searchParams.get("q") ?? "").trim();
-  if (!query) {
-    noSearchDefaultPageRender();
-    return null;
-  }
-
-  const bangs = await fetchBangs();
-
-  const match = query.match(/!(\S+)/i);
-
-  let bangCandidate;
-  if (match) {
-    bangCandidate = match[1].toLowerCase();
-  } else {
-    bangCandidate = localStorage.getItem("default-bang") ?? "g";
-  }
-  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
-
-  // Remove the first bang from the query
-  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
-
-  // If the query is just `!gh`, use `github.com` instead of `github.com/search?q=`
-  if (cleanQuery === "")
-    return selectedBang ? `https://${selectedBang.d}` : null;
-
-  // Format of the url is:
-  // https://www.google.com/search?q={{{s}}}
-  const searchUrl = selectedBang.u.replace(
-    "{{{s}}}",
-    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
-    encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
-  );
-  if (!searchUrl) return null;
-
-  return searchUrl;
-}
-
-async function registerServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("/sw.js", {
-        scope: "/",
-      });
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
-    }
-  }
-};
-
-async function pageLoaded() {
-  noSearchDefaultPageRender();
-  registerServiceWorker();
-}
-
-async function redirectToSearch() {
-  const searchUrl = await getBangredirectUrl();
-  if (!searchUrl) return;
-  window.location.replace(searchUrl);
-}
-
-window.addEventListener("load", pageLoaded);
-redirectToSearch();
+window.addEventListener("load", noSearchDefaultPageRender);
 
--- sw.js
+++ sw.js
@@ -7,18 +7,67 @@ const addResourcesToCache = async (resources) => {
 self.addEventListener("install", (event) => {
   event.waitUntil(
     addResourcesToCache([
-      "/",
-      "/index.html",
-      "/src/global.css",
-      "/src/main.js",
       "/assets/bangs.json",
-      "/assets/clipboard-check.svg",
-      "/assets/clipboard.svg",
-      "/assets/duck.svg",
     ]),
   );
 });
 
 self.addEventListener("fetch", async (event) => {
-  event.respondWith(caches.match(event.request, { ignoreSearch: true }));
+  event.respondWith(getResponse(event.request))
 });
+
+async function getResponse(request) {
+  const url = new URL(request.url);
+  const query = (url.searchParams.get("q") ?? "").trim();
+  if (!query) {
+    return await fetch(request);
+  }
+  const bangsResponse = await caches.match("/assets/bangs.json");
+  const bangsBlob = await bangsResponse.blob();
+  const bangs = JSON.parse(await bangsBlob.text());
+  const match = query.match(/!(\S+)/i);
+
+  let bangCandidate;
+  if (match) {
+    bangCandidate = match[1].toLowerCase();
+  } else {
+    bangCandidate = "g";
+  }
+  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+
+  // Remove the first bang from the query
+  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
+
+  // If the query is just `!gh`, use `github.com` instead of `github.com/search?q=`
+  if (cleanQuery === "")
+    return selectedBang ? `https://${selectedBang.d}` : null;
+
+  // Format of the url is:
+  // https://www.google.com/search?q={{{s}}}
+  const searchUrl = selectedBang.u.replace(
+    "{{{s}}}",
+    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+    encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
+  );
+  if (!searchUrl) {
+    return (
+      new Response(`searchUrl is ${searchUrl}>`, {
+        status: 500,
+        headers: { 
+          "Content-Type": "text/plain",
+        },
+      })
+    );
+  }
+
+  return (
+    new Response(`<a href="${searchUrl}>">Search!</a>`, {
+      status: 301,
+      headers: { 
+        "Content-Type": "text/html",
+        "Location": searchUrl
+      },
+    })
+  );
+
+}
